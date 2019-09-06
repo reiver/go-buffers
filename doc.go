2200524498fac631b44dc8c7b37f96192a87cf04
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-09-05 18:31:11 -0700

    docs

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
 /*
 Package buffers provides tools for working with byte array, and byte slice buffers, for the Go programming language.
 
+Example
+
+Here is an example of buffers.Writer being used to provide a io.Writer interface to an byte array, and byte slice:
+
 	import "github.com/reiver/go-buffers"
 	
 	// ...
